43146d7c28ba9a1bb457bfb08ac0f64c2a40956c
l1nux-th1ngz <137152321+l1nux-th1ngz@users.noreply.github.com>
2025-07-24 07:05:56 -0600

    Update 1-bspwm.sh

--- 1-bspwm.sh
+++ 1-bspwm.sh
@@ -4,48 +4,38 @@
 sudo apt update
 
 # Build Tools
-sudo apt install -y \
-  build-essential make gcc cmake bison meson ninja-build curl pkg-config wget git gnupg xauth \
-  zip unzip autoconf autoconf-archive automake autotools-dev gettext autorandr bash-completion \
-  libtool-bin libtool clangd intltool intltool-debian systemd-sysv sysvinit-core \
-  libnss-systemd libpam-systemd
+sudo apt install build-essential make gcc cmake bison meson ninja-build curl pkg-config wget git  gnupg xauth -y
+sudo apt install zip unzip autoconf autoconf-archive automake autotools-dev gettext autorandr bash-completions -y
+sudo apt install libtool-bin libtool pkg-config clangd intltool intltool-debian systemd-sysv sysvinit-core -y
+sudo apt install libnss-systemd libpam-systemd -y
 
-# Display Server
-sudo apt install -y \
-  xinit xorg xorg-dev x11-utils x11-xkb-utils x11-xserver-utils \
-  xserver-xorg-core xserver-xorg-video-intel xserver-xorg-input-libinput
+# Display server
+sudo apt install xinit  xorg xorg-dev x11-utils x11-xkb-utils x11-xserver-utils  -y
+sudo apt install xserver-xorg-core xserver-xorg-video-intel xserver-xorg-input-libinput -y
 
-# X Authority + Profile
-touch ~/.Xauthority
-cp /etc/X11/xinit/xinitrc ~/.xinitrc
-cp /etc/profile ~/.profile
+# Dependcies
+sudo apt install xbindkeys xbacklight xbindkeys xvkbd xinput  libx11-dev libxft-dev libxinerama-dev -y
+sudo apt install xcb libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-icccm4-dev -y
+sudo apt install libxcb-keysyms1-dev libxcb-xinerama0-dev libasound2-dev libxcb-xtest0-dev libxcb-shape0-dev  -y
 
-# Take Ownership
-sudo chown "$USER:$USER" ~/.profile ~/.xinitrc ~/.Xauthority
+# Processor
+sudo apt install intel-microcode -y
 
-# Dependencies
-sudo apt install -y \
-  xbindkeys xbacklight xvkbd xinput libx11-dev libxft-dev libxinerama-dev \
-  libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-icccm4-dev \
-  libxcb-keysyms1-dev libxcb-xinerama0-dev libasound2-dev libxcb-xtest0-dev libxcb-shape0-dev
+# Bspwm
+sudo apt install init bspwm sxhkd suckless-tools xdo xdotool inxi -y
+sudo apt install xautomation xsettingsd wmctrl rxvt-unicode intltool-debian -y
+sudo apt install dunst feh lxappearance mtools samba-client cifs-utils terminator -y
+sudo apt install nemo dialog feh lxappearance network-manager-gnome mirage -y
+sudo apt install dunst avahi-daemon intltool acpi acpid jq -y
+sudo apt install trash-cli brightnessctl light geany-plugins bluefish -y
+sudo apt install silversearcher-ag fd-find vs ripgrep gvfs-backends -y
+sudo apt install rofi-dev polybar xq yq -y
 
-# ⚙ Processor
-sudo apt install -y intel-microcode
+# XFCE Power no recommends
+sudo apt install xfce4-power-manager -o APT::Install-Recommends=false -y
 
-# Bspwm + Utilities
-sudo apt install -y \
-  init bspwm sxhkd suckless-tools xdo xdotool inxi xautomation xsettingsd wmctrl \
-  rxvt-unicode dunst feh lxappearance mtools samba-client cifs-utils terminator \
-  nemo dialog network-manager-gnome mirage avahi-daemon acpi acpid xdg-user-dir-gtk jq \
-  trash-cli brightnessctl light geany-plugins bluefish silversearcher-ag fd-find vs ripgrep \
-  gvfs-backends rofi-dev polybar xq yq qt5ct qt6ct
-
-# Power Manager
-sudo apt install -y xfce4-power-manager -o APT::Install-Recommends=false
-
-# Launch Power Manager
-# xfce4-power-manager &
-
+sudo apt install xdg-user-dir-gtk -y
+sleep 3
 # Setup User Directories
 xdg-user-dirs-update
 sleep 3
@@ -54,6 +44,4 @@ sleep 3
 
 # Wait for everything to finsish
 wait
-
-echo "Setup complete. Please continue to the next script."
-
+echo "Please continue to the next script"
