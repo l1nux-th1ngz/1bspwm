2984ee0836515f2c19b6c1c76b7cb2d268e3d105
l1nux-th1ngz <137152321+l1nux-th1ngz@users.noreply.github.com>
2025-07-24 01:30:50 -0600

    Add files via upload

---
+++ mv-dirs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Move directories
+mv {bspwm, dmenu, dunst, gtk-2.0, gtk-3.0, polybar, qt5ct, qt6ct, rofi, sxhkd, geany}  .config
+
+# Set permissions
+chmod -R 755 .config/{bspwm,dmenu,dunst,polybar,rofi,sxhkd}
+
+echo "Done"
