059c4bd7c38af7e9e6f94245c719ec6a10a29300
l1nux-th1ngz <137152321+l1nux-th1ngz@users.noreply.github.com>
2025-07-24 02:51:55 -0600

    Update mv-dirs.sh

--- mv-dirs.sh
+++ mv-dirs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Move directories
-sudo mv {bspwm,dmenu,dunst,gtk-2.0,gtk-3.0,polybar,qt5ct,qt6ct,rofi,sxhkd,geany} ~/.config/
+mv {bspwm,dmenu,dunst,gtk-2.0,gtk-3.0,polybar,qt5ct,qt6ct,rofi,sxhkd,geany} ~/.config/
 
 # Set permissions
 chmod -R 777 ~/.config/{bspwm,dmenu,dunst,polybar,rofi,sxhkd}
