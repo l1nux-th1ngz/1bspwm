b5ff6643ec6fc88c4f174811f8ace9413dea965a
l1nux-th1ngz <137152321+l1nux-th1ngz@users.noreply.github.com>
2025-07-24 06:22:42 -0600

    Update and rename mv-dirs.sh to mvdirs.sh

--- mvdirs.sh
+++ mvdirs.sh
@@ -6,4 +6,10 @@ mv {bspwm,dmenu,dunst,gtk-2.0,gtk-3.0,polybar,qt5ct,qt6ct,rofi,sxhkd,geany} ~/.c
 # Set permissions
 chmod -R 777 ~/.config/{bspwm,dmenu,dunst,polybar,rofi,sxhkd}
 
+# Prompt for username
+read -p "Enter your username: " username
+
+# Change ownership
+chown -R "$username:$username" ~/.config/{bspwm,dmenu,dunst,gtk-2.0,gtk-3.0,polybar,qt5ct,qt6ct,rofi,sxhkd,geany}
+
 echo "Done"
